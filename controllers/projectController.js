aba6336958bf44bfa5e58d92ec5da6c446f6c3a8
Piyu HP Binu <mr.binara@gmail.com>
2026-06-08 08:31:57 +1000

    controller adjustment

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,7 +3,20 @@ const Project = require("../models/Project");
 // GET all projects
 exports.getProjects = async (req, res) => {
   const projects = await Project.find();
-  res.json(projects);
+
+  const sortedProjects = projects.sort((a, b) => {
+    const defaultVideo = "/projects/videos/card-videos/project-video.mp4";
+
+    const aIsDefault = a.cardVideo === defaultVideo;
+    const bIsDefault = b.cardVideo === defaultVideo;
+
+    if (aIsDefault && !bIsDefault) return 1;
+    if (!aIsDefault && bIsDefault) return -1;
+
+    return 0;
+  });
+
+  res.json(sortedProjects);
 };
 
 // GET single project
